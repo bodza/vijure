c0a599b59258300655c9fc15d3eabdc2e34d3fde
Robert Henryk Zawiasa <robert.zawiasa@gmail.com>
2017-04-22 10:02:47 +0200

    60530a

---
+++ project.clj
@@ -0,0 +1,12 @@
+(defproject vijure "x.y.z"
+    :dependencies [[org.clojure/clojure "1.8.0"]
+                   [org.clojure/core.async "0.2.374"]
+                   [com.github.jnr/jnr-ffi "2.0.9"]
+                 #_[com.github.jnr/jffi #_"1.2.8" "1.2.12"]
+                 #_[org.ow2.asm/asm #_"5.0.4" "5.1"]]
+    :plugins [[lein-try "0.4.3"]
+            #_[venantius/ultra "0.4.1"]]
+;   :global-vars {*warn-on-reflection* true}
+    :jvm-opts ["-Xmx12g"]
+;   :javac-options ["-g"]
+    :source-paths ["src"] :java-source-paths ["src"] :resource-paths ["resources"] :test-paths ["src"])
